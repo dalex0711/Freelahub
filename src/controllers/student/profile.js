ca502121775184782d16ccffecb4a90673789fed
Dalex <dalexasanjuan@outlook.com>
2025-07-20 21:47:29 -0500

    vista estudiante profile

--- src/controllers/student/profile.js
+++ src/controllers/student/profile.js
@@ -6,7 +6,6 @@ import { createIcons, icons } from 'lucide';
 
 export async function init() {
    createIcons({ icons });
-   const userDetails = await apiRequest('GET', `profiles?userId=${user.id}`);
   if (location.search === '') {
     const user = getUserLogged();
     const userDetails = await apiRequest('GET', `profiles?userId=${user.id}`);
@@ -14,7 +13,8 @@ export async function init() {
     if (userDetails.length === 0) {
       userDetailsForm(user);
     }else{
-         renderProfile(userDetails)
+        document.getElementById('main-header').classList.remove('hidden');
+        renderProfile(userDetails[0])
     }
   }
 }
@@ -31,14 +31,16 @@ function userDetailsForm(user) {
     const formData = new FormData(form);
     const detailData = {
       userId: user.id,
-      nombre: formData.get('nombre'),
-      telefono: formData.get('telefono'),
-      ciudad: formData.get('ciudad'),
-      institucion: formData.get('institucion'),
-      programa: formData.get('programa'),
-      habilidades: formData.get('habilidades').split(',').map(h => h.trim()),
+      name: formData.get('name'),
+      cellPhone: formData.get('cellPhone'), 
+      mail: formData.get('mail'),
+      city: formData.get('city'),
+      institution: formData.get('institution'),
+      program: formData.get('program'),
+      skills: formData.get('skills').split(',').map(h => h.trim()),
+      languages : formData.get('languages').split(',').map(h => h.trim()),
       descripcion : formData.get('descripcion'),
-      cvUrl: formData.get('cvUrl')
+      cvUrl: formData.get('cvFile')
     };
     
     await apiRequest('POST','profiles', detailData)
@@ -46,9 +48,38 @@ function userDetailsForm(user) {
     }
 )}
 
+function renderProfile(data){
+   
+    const template = document.getElementById('template-container').content.cloneNode(true);
+    template.getElementById('avatar').src=`https://ui-avatars.com/api/?name=${data.name}&color=1f2937&background=f3f4f6&size=120`;
+    template.getElementById('name').textContent = data.name;
+    template.getElementById('cellPhone').textContent = data.cellPhone;
+    template.getElementById('mail').textContent = data.mail;
+    template.getElementById('city').textContent = data.city;
+    template.getElementById('institution').textContent = data.institution;
+    template.getElementById('program').textContent = data.program;
+    template.getElementById('descripcion').textContent = data.descripcion;
 
 
-function renderProfile(userDetails){
+    const skillsT = template.getElementById('skills');
+    data.skills.forEach(skill => {
+        const li = document.createElement('li');
+        li.textContent = skill;
+        li.className = 'bg-gray-100 text-gray-700 px-3 py-1 rounded-full';
+        
+        skillsT.appendChild(li);
+    }); 
+
+    const languagesT = template.getElementById('languages');
+    data.languages.forEach(language => {
+    const li = document.createElement('li');
+    li.className = "bg-gray-100 px-3 py-1 rounded-full";
+    li.textContent = language;
+    languagesT.appendChild(li);
+    });
+
+    document.getElementById('profile-container').appendChild(template);
+    createIcons({ icons });
 
 }
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import './style.css';
 import { navegation,navegationTag} from './router.js';
 
+import { createIcons, icons } from 'lucide';
 
 
 document.addEventListener('DOMContentLoaded', () => {
   navegation(location.pathname); 
   navegationTag();               
-  
+    createIcons({ icons });
  
 });
